452099e16de9b7c147124249c5382545aff9a4b9
kiruiadrian <adriankirui4013@gmail.com>
2025-02-10 09:19:43 +0300

    e-soma
    e-soma
    
    Co-Authored-By: kiuiadrian <adriankirui4013@gmail.com>

--- engine/settings/jazzmin.py
+++ engine/settings/jazzmin.py
@@ -12,7 +12,7 @@ JAZZMIN_SETTINGS = {
     'welcome_sign': 'Welcome to E-Soma ',
 
     # Copyright on the footer
-    'copyright': 'Antonnifo',
+    'copyright': 'KiruiAdrian',
 
     # The model admin to search from the search bar, search bar omitted if excluded
     'search_model': 'courses.Course',
